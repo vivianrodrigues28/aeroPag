b3df26d83eecffa3c3b1a7829aa21b5c0131fef6
Camile Vitória Gomes <camile.vitoria@escolar.ifrn.edu.br>
2024-10-29 08:47:02 -0300

    paginas

--- AEROPAG/cadastros/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Turma, Atividade
-# Register your models here.
-admin.site.register(Turma)
-admin.site.register(Atividade)
--- oi/cadastros/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Lembrete
-
-# Register your models here.
-admin.site.register(Lembrete)
