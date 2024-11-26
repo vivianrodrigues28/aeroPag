708a38ddba9b10a4afd25ff3547dd5f611fae37e
Vivian <maria.vivian@escolar.ifrn.edu.br>
2024-11-26 07:46:44 -0300

    atualizado

--- aeropag/cobrancas/forms.py
+++ aeropag/cobrancas/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from .models import Cobranca
-
-from django import forms
-from .models import Cobranca
+from .models import Cobranca, Tarifa
 
 class CobrancaForm(forms.ModelForm):
     class Meta:
@@ -13,4 +10,4 @@ class CobrancaForm(forms.ModelForm):
             'avi_codigo': forms.TextInput(attrs={'class': 'form-control'}),
             'quantidade_horas': forms.NumberInput(attrs={'class': 'form-control'}),
         }
-
+tar_codigo = forms.ModelChoiceField(queryset=Tarifa.objects.all(), empty_label="Selecione a Tarifa", widget=forms.Select(attrs={'class': 'form-control'}))
